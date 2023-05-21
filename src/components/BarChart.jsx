e9d89c2bb32b699a67b6103e99a39ec72220e252
Kamil <kamil.walczak@cobrick.com>
2023-05-21 09:19:45 +0200

    init commit

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import * as d3 from 'd3'
+
+export const BarChart = () => {
+
+  const data = [12, 5, 6,9, 10]
+
+  useEffect(() => {
+    // drawChart()
+  }, [])
+
+  return (
+    <div>BarChart</div>
+  )
+}
