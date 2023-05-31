941c2758468b80cfa0a6a10bd7f05ccad9881786
Kamil <kamil.walczak@cobrick.com>
2023-05-31 08:30:58 +0200

    Add axes

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import * as d3 from 'd3'
+import { data } from '../data'
 
 const CHART_WIDTH = 600
 const CHART_HEIGHT = 400
@@ -8,51 +9,6 @@ const START = 0
 const END = 30
 
 export const BarChart = () => {
-
-  const data = [
-    {
-      name: 'first',
-      start: 12, 
-      end: 25,
-      type: 'A'
-    },
-    {
-      name: 'third',
-      start: 6, 
-      end: 22,
-      type: 'A'
-    },
-    {
-      name: 'second',
-      start: 5, 
-      end: 10,
-      type: 'B'
-    },
-    {
-      name: 'second',
-      start: 1, 
-      end: 4,
-      type: 'C'
-    },
-    {
-      name: 'second',
-      start: 9, 
-      end: 10,
-      type: 'C'
-    },
-    {
-      name: 'first',
-      start: 10, 
-      end: 15,
-      type: 'B'
-    },
-    {
-      name: 'third',
-      start: 11, 
-      end: 14,
-      type: 'B'
-    }
-  ]
 
   const compare = (key) => (a, b) => {
     if (a[key] < b[key]) {
@@ -64,7 +20,6 @@ export const BarChart = () => {
     return 0
   }
 
-  
   const drawChart = () => {
     
     data.sort(compare('start'))
@@ -76,7 +31,7 @@ export const BarChart = () => {
     
     const yScale = d3.scaleBand()
       .domain(data.map((d) => d.name))
-      .range([0, CHART_HEIGHT])
+      .range([CHART_HEIGHT - 50, 0])
 
 
   
@@ -85,11 +40,26 @@ export const BarChart = () => {
       .attr('height', CHART_HEIGHT)
       .style('background-color', 'lightgrey')
 
-    const wrapper = svg.append('g').attr('class', 'bars')
+    svg.selectAll("*").remove()
+
+    const container = svg.append('g')
+      .attr('class', 'container')
+      .attr('transform', `translate(50, 0)`)
+      .attr('width', CHART_WIDTH - 50)
+      .attr('height', CHART_HEIGHT - 50)
+
+
+    const barsWrapper = container.append('g').attr('class', 'bars')
+    const xAxis = container.append('g').attr('class', 'xaxis')
+    const yAxis = container.append('g').attr('class', 'yaxis')
     
-    const bars = wrapper.selectAll('rect')
+    const bars = barsWrapper.selectAll('g')
       .data(data)
       .enter()
+      .append('g')
+      .attr('class', 'barWrapper')
+
+    bars
       .append('rect')
       .attr('y', (d, i) => yScale(d.name))
       .attr('x', (d, i) => xScale( d.start))
@@ -98,11 +68,25 @@ export const BarChart = () => {
       .attr('fill', (d) => d.type === 'A' ? 'red' : d.type === 'B' ? 'blue' : 'green')
       .attr('class', d => `bar ${d.name} ${d.type}`)
     
-    bars.on('mouseover', (e) => {
-      d3.select(e.target).raise()
-    })
-    .on('mouseout', () => bars.order()
-    )
+    bars
+      .append('text')
+      .attr('x', (d, i) => xScale(d.start) + 5)
+      .attr('y', (d, i) => yScale(d.name) + 15)
+      .text((d) => `${d.start} - ${d.end}`)
+      .attr('fill', 'white')
+      .attr('font-size', 10)
+
+      bars.on('mouseenter', (e) => {
+        // console.log(e.target)
+        d3.select(e.target).raise()
+      })
+      .on('mouseout', () => bars.order()
+      )
+    
+
+    xAxis.call(d3.axisBottom(xScale)).attr('transform', `translate(0, ${CHART_HEIGHT - 50})`)
+    yAxis.call(d3.axisLeft(yScale))//.attr('transform', `translate(50, 0)`)
+
   }
 
   useEffect(() => {
---
+++ src/data.js
@@ -0,0 +1,68 @@
+export   const data = [
+  {
+    name: 'first',
+    start: 12, 
+    end: 25,
+    type: 'A'
+  },
+  {
+    name: 'third',
+    start: 6, 
+    end: 22,
+    type: 'A'
+  },
+  {
+    name: 'second',
+    start: 5, 
+    end: 10,
+    type: 'B'
+  },
+  {
+    name: 'second',
+    start: 1, 
+    end: 4,
+    type: 'C'
+  },
+  {
+    name: 'second',
+    start: 9, 
+    end: 11,
+    type: 'C'
+  },
+  {
+    name: 'first',
+    start: 10, 
+    end: 15,
+    type: 'B'
+  },
+  {
+    name: 'fifth',
+    start: 2, 
+    end: 9,
+    type: 'B'
+  },
+  {
+    name: 'third',
+    start: 11, 
+    end: 14,
+    type: 'B'
+  },
+  {
+    name: 'fourth',
+    start: 3, 
+    end: 21,
+    type: 'A'
+  },
+  {
+    name: 'fifth',
+    start: 13, 
+    end: 18,
+    type: 'C'
+  },
+  {
+    name: 'fifth',
+    start: 16,
+    end: 22,
+    type: 'B'
+  }
+]
